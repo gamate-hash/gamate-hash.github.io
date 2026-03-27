36685efce201066abfc83e61dd21641beed71c38
giggityfiles <giggityfiles@gmail.com>
2026-03-27 18:38:32 -0400

    fot

--- pizda.user.js
+++ pizda.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BOMB PIZDA
 // @namespace    http://tampermonkey.net/
-// @version      03.20.2026.1
+// @version      03.27.2026.1
 // @description  Try to take over some sites!
 // @author       GAMATE HASH
 // @match        *://*/*
